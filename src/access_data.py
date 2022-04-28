e8ec7786108a55e7a43995265e8136b21f1ff159
joelosw <joel.oswald@psiori.com>
2022-04-28 13:35:49 +0200

    update how to get metadata

--- src/access_data.py
+++ src/access_data.py
@@ -8,6 +8,24 @@ config.read(configFilePath)
 reichsanzeiger_db = mysql.connector.connect(
     host='localhost',
     user=config.get("credentials", "username"),
-    password=config.get("credentials", "password")
+    password=config.get("credentials", "password"),
+    database="reichsanzeiger"
 )
 print(reichsanzeiger_db)
+
+cursor = reichsanzeiger_db.cursor(buffered=True)
+
+cursor.execute("SHOW TABLES;")
+tables = cursor.fetchall()
+print("Availabke Tables")
+for table_name in tables:
+    print("-----"+table_name[0]+"-------")
+    print('Example Row:')
+    
+    cursor.execute(f"SHOW COLUMNS FROM {table_name[0]};")
+    row_fields = cursor.fetchall()
+    print([row[0] for row in row_fields])
+
+    cursor.execute(f"SELECT * FROM {table_name[0]};")
+    row = cursor.fetchone()
+    print(row)
