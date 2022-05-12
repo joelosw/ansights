0d58a194d74a9df11ee1d3bf0b0f21897a3a98ee
uerib <simeon.allmendinger@student.kit.edu>
2022-05-12 11:19:25 +0200

    add tesseract

--- src/01_OCR/tesseract.py
+++ src/01_OCR/tesseract.py
@@ -1,72 +1,67 @@
 from PIL import Image
 from cleantext import clean
 from spellchecker import SpellChecker
+from textblob import TextBlob
 
 import pytesseract
 
-TEST_PATH = '../../data/2013_0473_031__ansicht03.tif'
+# 'data/2013_0473_031__ansicht03.tif'
+TEST_PATH = 'data/2013_0473_029__ansicht01.tif'
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'tesseract'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+# pytesseract.pytesseract.tesseract_cmd = r'tesseract'
+# print(pytesseract.get_languages(config=''))
 
-# Simple image to string
-text = pytesseract.image_to_string(Image.open(TEST_PATH))
-print(text[:100])
+def get_string(IMAGE_PATH, debug=False):
+    # Simple image to string
+    text = pytesseract.image_to_string(Image.open(IMAGE_PATH), lang='deu')
+    # text = TextBlob(text)
+    
+    if debug:
+        print(text)
 
-text_cleaned = clean(text[:100])
-spell = SpellChecker('de')
+    return text
 
-# find those words that may be misspelled
-# misspelled = spell.unknown(text_cleaned.split())
-text_prep = ''
+def clean_text(text):
+    text_cleaned = clean(text[:])
+    return text_cleaned
 
-for word in text_cleaned.split():
-    # Get the one `most likely` answer
-    print(word)
-    print(spell.unknown(word))
-    if spell.unknown(word) != spell.unknown('Bro'):
-        print(spell.unknown(word))
-        # print(spell.correction(word))
-        text_prep = text_prep + spell.correction(word) + ' '
-    else:
-        text_prep = text_prep + word + ' '
+def init_SpellChecker():
+    spell = SpellChecker('de')
+    return spell
 
+def enhance_text(spell, text_cleaned, debug=False):
+    text_preprrocessed = ''
 
-    # Get a list of `likely` options
-    # print(spell.candidates(word))
+    for word in text_cleaned.split():
+        if debug:
+            print(word)
 
-print(text_prep)
+        # find those words that may be misspelled
+        if spell.unknown([word]) != set():
+            
+            if debug:
+                print(spell.candidates(word))
+            
+            word_variant = word.replace("f", "s")
+            word_variant = word_variant.replace("F", "S")
 
+            if spell.known([word_variant]) != set():
+                text_preprrocessed = text_preprrocessed + word_variant + ' '
+                continue
+            
+            text_preprrocessed = text_preprrocessed + spell.correction(word) + ' '
+        else:
+            text_preprrocessed = text_preprrocessed + word + ' '
 
+    print(text_preprrocessed)
 
-# Batch processing with a single file containing the list of multiple image file paths
-# print(pytesseract.image_to_string('images.txt'))
+def main(debug=False):
+    text = get_string(IMAGE_PATH=TEST_PATH, debug=debug)
+    text_cleaned = clean_text(text)
+    spell = init_SpellChecker()
+    text_preprocessed = enhance_text(spell=spell, text_cleaned=text_cleaned, debug=debug)
+    
 
-# Timeout/terminate the tesseract job after a period of time
-'''try:
-    print(pytesseract.image_to_string(TEST_PATH, timeout=2)) # Timeout after 2 seconds
-    print(pytesseract.image_to_string(TEST_PATH, timeout=0.5)) # Timeout after half a second
-except RuntimeError as timeout_error:
-    # Tesseract processing is terminated
-    pass
-
-# Get bounding box estimates
-# print(pytesseract.image_to_boxes(Image.open(TEST_PATH)))
-
-# Get verbose data including boxes, confidences, line and page numbers
-# print(pytesseract.image_to_data(Image.open(TEST_PATH)))
-
-# Get information about orientation and script detection
-#print(pytesseract.image_to_osd(Image.open(TEST_PATH)))
-
-# Get a searchable PDF
-#pdf = pytesseract.image_to_pdf_or_hocr('test.png', extension='pdf')
-#with open('test.pdf', 'w+b') as f:
-    #f.write(pdf) # pdf type is bytes by default
-
-# Get HOCR output
-#hocr = pytesseract.image_to_pdf_or_hocr('test.png', extension='hocr')
-
-# Get ALTO XML output
-#xml = pytesseract.image_to_alto_xml('test.png')'''
+if __name__ == '__main__':
+    main(debug=False)
