5d8a7cf27c6cc2a1bc73719bd6d6c4f65b5e19cf
joelosw <joel.oswald@psiori.com>
2022-05-16 16:37:33 +0200

    deebug tesserract steps

--- src/01_OCR/tesseract.py
+++ src/01_OCR/tesseract.py
@@ -14,23 +14,28 @@ TEST_PATH = 'data/2013_0473_023__ansicht01.tif'
 # pytesseract.pytesseract.tesseract_cmd = r'tesseract'
 # print(pytesseract.get_languages(config=''))
 
-def get_string(IMAGE_PATH, lang='deu',debug=False):
+
+def get_string(IMAGE_PATH, lang='deu', debug=False):
     # Simple image to string
-    text = pytesseract.image_to_string(Image.open(IMAGE_PATH), lang=lang) # 'deu'
+    text = pytesseract.image_to_string(
+        Image.open(IMAGE_PATH), lang=lang)  # 'deu'
     # text = TextBlob(text)
-    
+
     if debug:
         print(text)
 
     return text
 
+
 def clean_text(text):
     text_cleaned = clean(text[:])
     return text_cleaned
 
+
 def init_SpellChecker():
     spell = SpellChecker('de')
     return spell
+
 
 def enhance_text(spell, text_cleaned, debug=False):
     text_preprrocessed = ''
@@ -41,33 +46,42 @@ def enhance_text(spell, text_cleaned, debug=False):
 
         # find those words that may be misspelled
         if spell.unknown([word]) != set():
-            
+
             if debug:
                 print(spell.candidates(word))
-            
+
             word_variant = word.replace("f", "s")
             word_variant = word_variant.replace("F", "S")
 
             if spell.known([word_variant]) != set():
                 text_preprrocessed = text_preprrocessed + word_variant + ' '
                 continue
-            
-            text_preprrocessed = text_preprrocessed + spell.correction(word) + ' '
+
+            text_preprrocessed = text_preprrocessed + \
+                spell.correction(word) + ' '
         else:
             text_preprrocessed = text_preprrocessed + word + ' '
 
     print(text_preprrocessed)
 
+
 def get_text_dict(languages=['deu', 'deu_frak'], IMAGE_PATH=TEST_PATH, debug=False):
     text_strings = {}
     for lang in languages:
-        text = get_string(IMAGE_PATH=IMAGE_PATH, lang=lang,debug=debug)
+        print(f'===== LANGUAGE: {lang} =========')
+        text = get_string(IMAGE_PATH=IMAGE_PATH, lang=lang, debug=debug)
+        print(f'----Pure Text----- \n {text}')
         text_cleaned = clean_text(text)
+        print(f'----Cleaned Text----- \n {text_cleaned}')
         spell = init_SpellChecker()
-        text_preprocessed = enhance_text(spell=spell, text_cleaned=text_cleaned, debug=debug)
+        text_preprocessed = enhance_text(
+            spell=spell, text_cleaned=text_cleaned, debug=debug)
+        print(f'----Final Text----- \n {text_preprocessed}')
         text_strings[lang] = text_preprocessed
-    
+
     return text_strings
 
+
 if __name__ == '__main__':
-    print(get_text_dict(languages=['deu', 'deu_frak'], IMAGE_PATH=TEST_PATH, debug=False))
+    get_text_dict(
+        languages=['deu', 'deu_frak'], IMAGE_PATH=TEST_PATH, debug=False)
