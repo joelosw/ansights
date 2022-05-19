904501a22ee5bb103c934301677b9f969a6a096c
uerib <simeon.allmendinger@student.kit.edu>
2022-05-20 01:34:59 +0200

    update lobid-api

--- src/03_GND/lobid_api.py
+++ src/03_GND/lobid_api.py
@@ -8,21 +8,21 @@ import requests
 import pandas as pd
 
 
-def get_gnd_json(query: str, sru_server='https://lobid.org/', catalog='gnd/search', 
+def get_gnd_json(query: str, lobid_server='https://lobid.org/', catalog='gnd/search', 
                  filter='+(type:SubjectHeading)', verbose=False):
     """
         The get_gnd_json function accepts a query, URL components and filters as arguments and 
         returns the json file of the data item at a particular URL.
 
         :param query:str: Used to pass the keywords of our search.
-        :param sru_server='https://lobid.org/': Used to pass the base url of the page that is being scraped.
+        :param lobid_server='https://lobid.org/': Used to pass the base url of the page that is being scraped.
         :param catalog='gnd/search': Used to pass the catalog of the page that is being scraped.
         :param filter='+(type:SubjectHeading)': Used to add filters to our search.
         :param verbose=False: Used to print out relevant information for debugging.
         :return: A json.
     """
 
-    base_url = sru_server + catalog
+    base_url = lobid_server + catalog
     params = {'q': query,
               'filter': filter,
               'format': 'json'
@@ -32,21 +32,27 @@ def get_gnd_json(query: str, sru_server='https://lobid.org/', catalog='gnd/searc
     json_query=r.json()
 
     if verbose:
+        print(json_query['id'])
         print('JSON File created')
     
     return json_query
 
 
-def get_gnd_keywordRelations(keywords :list, json_keys=['relatedTerm', 'gndSubjectCategory'], 
-                             max_items=10, print_output=True, verbose=True):
+def get_gnd_keywordRelations(keywords :list, max_items=10, print_output=True, verbose=True, 
+                             remove_double=True, json_keys=['relatedTerm', 'gndSubjectCategory', 
+                                                            'relatedPlaceOrGeographicName',
+                                                            'preferredName','broaderTermInstantial',
+                                                            'broaderTermGeneral', 'variantName']):
     
     """
         The get_gnd_keywordRelations function accepts keywords and keys as arguments and 
         returns the corrresponding output of the gnd for each keyword given a particular key in 
         a pandas dataframe.
 
-        :param keywords :list: Used to pass the keywords of our search.
-        :param json_keys=['relatedTerm', 'gndSubjectCategory']: Used to pass the keys to scrape for linked terms.
+        :param keywords :list: Used to pass the keywords of our search in list.
+        :param json_keys==['relatedTerm', 'gndSubjectCategory', 'relatedPlaceOrGeographicName',
+                           'preferredName','broaderTermInstantial', 'broaderTermGeneral', 
+                           'variantName']: Used to pass the keys to scrape for linked terms.
         :param max_items=10: Used to pass maximal number of items for each key.
         :param print_output=True: Used to print the dataframe for each keyword.
         :param verbose=False: Used to print out relevant information for debugging.
@@ -79,15 +85,42 @@ def get_gnd_keywordRelations(keywords :list, json_keys=['relatedTerm', 'gndSubje
                         related_terms = element['label']
                         
                         for term in related_terms.split(','):
-                            df[keyword][0][json_key].append(term.strip(' '))
+                            stripped_term=term.strip(' ')
+                            df[keyword][0][json_key].append(stripped_term)
 
                             if verbose:
-                                stripped_term=term.strip(' ')
                                 print(f'Related Term for keyword "{keyword}" is {stripped_term}')
 
                 except:
-                    if verbose:
-                        print(f'Member {item+1} of kewyowrd "{keyword}" has no {json_key}.')
+                    
+                    # If value of key is a list or string! No explicit label key
+                    try:
+                        
+                        for term in member[json_key].split(','):
+                            stripped_term=term.strip(' ')
+                            df[keyword][0][json_key].append(stripped_term)
+
+                            if verbose:
+                                print(f'Related Term for keyword "{keyword}" is {stripped_term}')
+
+                    except:
+
+                        try:
+
+                            for term in member[json_key]:
+                                df[keyword][0][json_key].append(term)
+
+                                if verbose:
+                                    print(f'Related Term for keyword "{keyword}" is {term}')
+
+                        except:
+
+                            if verbose:
+                                print(f'Member {item+1} of kewyowrd "{keyword}" has no {json_key}.')
+        
+                if remove_double:
+                    df[keyword][0][json_key]=list(dict.fromkeys(df[keyword][0][json_key]))
+
 
         if print_output:
             print()
@@ -97,6 +130,6 @@ def get_gnd_keywordRelations(keywords :list, json_keys=['relatedTerm', 'gndSubje
     return df
 
 
-
-keys=['Streik', 'Joel']
-df=get_gnd_keywordRelations(keywords=keys, json_keys=['relatedTerm', 'gndSubjectCategory'], print_output=True, verbose=False)
+# TEST
+keys=['Streik']
+df=get_gnd_keywordRelations(keywords=keys, max_items=10, print_output=True, verbose=False)
