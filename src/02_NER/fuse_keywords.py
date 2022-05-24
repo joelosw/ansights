8ab72e8f66d535c35f275e3e9b79efd99c7b9da6
joelosw <joel.oswald@psiori.com>
2022-05-24 08:11:43 +0200

    fix fuse_keywords

--- src/02_NER/fuse_keywords.py
+++ src/02_NER/fuse_keywords.py
@@ -1,3 +1,4 @@
+# requires python>3.9
 import os
 from spacy_test import extract_nouns_and_verbs, WordType
 from keyword_extractor import extract_keyword_score
@@ -21,27 +22,31 @@ ner_logger.addHandler(console)
 
 def fuse(text: str, max_nouns: int = None, max_verbs: int = None):
     scores = dict(extract_keyword_score(text))
+    ner_logger.debug(f' Scores: {scores}')
     types = extract_nouns_and_verbs(text)
     words_with_scores = {}
     prop_nouns = []
     for noun in types[WordType.PROP_NOUN]:
         try:
-            prop_nouns.append(noun, scores[noun])
-        except:
+            prop_nouns.append((noun, scores[noun]))
+        except KeyError as e:
+            print(e)
             ner_logger.warning(f'Could not get score for "{noun}"')
 
     nouns = []
     for noun in types[WordType.NOUN]:
         try:
-            nouns.append(noun, scores[noun])
-        except:
+            nouns.append((noun, scores[noun]))
+        except KeyError as e:
+            print(e)
             ner_logger.warning(f'Could not get score for "{noun}"')
 
     verbs = []
     for verb in types[WordType.VERB]:
         try:
-            verbs.append(verb, scores[verb])
-        except:
+            verbs.append((verb, scores[verb]))
+        except KeyError as e:
+            print(e)
             ner_logger.warning(f'Could not get score for "{verb}"')
 
     if max_nouns:
@@ -51,9 +56,9 @@ def fuse(text: str, max_nouns: int = None, max_verbs: int = None):
         num_nouns = len(nouns)
         candidates = []
         for i in range(max_nouns):
-            if num_prop >= i:
+            if num_prop > i:
                 candidates.append(prop_nouns[i])
-            elif num_nouns >= i - num_prop:
+            elif num_nouns > i - num_prop:
                 candidates.append(nouns[i - num_prop])
             else:
                 ner_logger.warning(
@@ -67,7 +72,7 @@ def fuse(text: str, max_nouns: int = None, max_verbs: int = None):
         num_verbs = len(verbs)
         candidates = []
         for i in range(max_verbs):
-            if num_verbs >= i:
+            if num_verbs > i:
                 candidates.append(verbs[i])
             else:
                 ner_logger.warning(
@@ -81,4 +86,4 @@ def fuse(text: str, max_nouns: int = None, max_verbs: int = None):
 if __name__ == '__main__':
     with open(os.path.join(repo_path, 'data', 'arbeiter_aufruf_OCR_uncleaned.txt'), 'r') as f:
         text = f.read()
-    print(fuse(text))
+    print(fuse(text, max_nouns=4, max_verbs=2))
