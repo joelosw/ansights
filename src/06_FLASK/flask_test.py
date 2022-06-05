ae0ec27db797ebe43c3e2b27929134bcc6359b73
uerib <simeon.allmendinger@student.kit.edu>
2022-06-06 00:41:06 +0200

    init App

--- src/06_FLASK/flask_test.py
+++ src/06_FLASK/flask_test.py
@@ -1,12 +1,11 @@
 import time
 from flask import Flask
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
-
-@app.route('/api/news_graph')
-def get_current_news_graph():
-    return send_file('src/05_Visualization/Models/VisualAnzeights.html')
