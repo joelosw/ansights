22c101cde662fb2ba648ecb77323dcc499658bd5
joelosw <joel.oswald@psiori.com>
2022-06-07 18:14:51 +0200

    allow color-grouping

--- src/05_Visualization/keyword_visual.py
+++ src/05_Visualization/keyword_visual.py
@@ -4,6 +4,8 @@ import numpy as np
 import webbrowser
 import os
 import random
+from sklearn.manifold import MDS
+from sklearn.preprocessing import MinMaxScaler
 sys.path.append('./')
 sys.path.append('./../')
 sys.path.append('./../..')
@@ -25,19 +27,47 @@ def calculate_similarity_matrix(news_pages, eps=1):
     return similarities
 
 
-def create_graph(news_pages, physics=True, paper=None, num_keywords=6):
-    #similarities = calculate_similarity_matrix(news_pages, eps=0.1)
+def calculate_jacard_matrix(news_pages, eps=1):
+    jaccard = np.zeros((len(news_pages), len(news_pages)))
+    for i in range(len(news_pages)):
+        for j in range(i):
+            jaccard[i, j] = len(news_pages[i].common_keywords(
+                news_pages[j]))/(len(news_pages[i].keywords | news_pages[j].keywords)+eps)
+            logger.debug(f'Dist between {i},{j} = {jaccard[i,j]}')
+
+    return jaccard
+
+
+def calculate_colors(similarity_matrix):
+    i_lower = np.tril_indices(similarity_matrix.shape[0], -1)
+    similarity_matrix.T[i_lower] = similarity_matrix[i_lower]
+    dissim = 1-similarity_matrix
+    np.fill_diagonal(dissim, 0)
+    embedding = MDS(n_components=3, dissimilarity='precomputed')
+    scaler = MinMaxScaler()
+    print(f'Dissimilarities:\n {dissim}')
+    three_d = embedding.fit_transform(dissim)
+    colors = (scaler.fit_transform(three_d)*255).astype(int)
+    return colors
+
+
+def create_graph(news_pages, num_keywords=6, color_keywords=False):
+    jaccard = calculate_jacard_matrix(news_pages, eps=0)
+    i_lower = np.tril_indices(jaccard.shape[0], -1)
+    jaccard.T[i_lower] = jaccard[i_lower]
+    np.fill_diagonal(jaccard, 1)
+    colors = calculate_colors(jaccard)
     net = Network(height='100%', width='75%')
+    net.inherit_edge_colors(True)
     net.barnes_hut()
     scan_images = os.listdir(os.path.join(repo_path, 'data', 'scan_examples'))
     scan_paths = ['file://' + os.path.join(
         repo_path, 'data', 'scan_examples', image) for image in scan_images]
-    if not paper is None:
-        net.add_node('KEY', size=10*num_keywords, title='Flugblatt', label='Flugblatt', shape='image',
-                     image='file:///Users/joel/Library/CloudStorage/OneDrive-Personal/_UNI/SS22/daVinci/data/example_flyer.jpg')
+    net.add_node('KEY', size=10*num_keywords, title='Flugblatt', label='Flugblatt', shape='image',
+                 image='file:///Users/joel/Library/CloudStorage/OneDrive-Personal/_UNI/SS22/daVinci/data/example_flyer.jpg')
     for i, page in enumerate(news_pages):
         net.add_node(i, size=10*len(page.keywords),
-                     title=page.name + '\n Keywords: \n' + '\n'.join(page.keywords), label=page.name.split('-')[-1], shape='image', image=random.choice(scan_paths))
+                     title=page.name + '\n Keywords: \n' + '\n'.join(page.keywords), label=page.name.split('-')[-1], shape='image', image=random.choice(scan_paths), color=f'rgba{tuple(colors[i,:])+(0.7,)}')
 
     for i in range(len(news_pages)):
         for j in range(i):
@@ -46,14 +76,30 @@ def create_graph(news_pages, physics=True, paper=None, num_keywords=6):
             #              length=(100*np.max(similarities)/dist), physics=physics)
             common_words = news_pages[i].common_keywords(news_pages[j])
             similarity = len(common_words)
+            jacc = jaccard[i, j]
+            weight = jacc
+            value = similarity
+            length = int(100*(1-jacc)) + 1
             if similarity > 0:
-                net.add_edge(i, j, weight=similarity,
-                             value=similarity, smooth=False, title=','.join(common_words), color='#83c5be', color_highlight='#ffddd2')
+                kwargs = dict(smooth=False,
+                              title=',\n'.join(
+                                  tuple(common_words)),
+                              weight=weight,
+                              value=value,
+                              length=length
+                              )
+
+                if not color_keywords:
+                    # Use default colors instead of inheritance
+                    kwargs['color'] = '#83c5be'
+                    kwargs['color.highlight'] = '#ffddd2'
+                    kwargs['color.hover'] = 'red'
+                net.add_edge(i, j,  **kwargs)
 
     for i, page in enumerate(news_pages):
         # Add in the end, so they are on top
-        net.add_edge('KEY', i, weight=len(page.keywords), value=len(
-            page.keywords), smooth=False, title=','.join(page.keywords), color='#006d77')
+        net.add_edge(i, 'KEY', weight=len(page.keywords), value=len(
+            page.keywords), smooth=False, title=','.join(page.keywords))  # , color='#006d77')
     return net
 
 
@@ -68,8 +114,10 @@ if __name__ == '__main__':
         relations = pickle.load(f)
         logger.info(
             f'{relations} \n  ...loaded cached {len(relations)} relations:')
+    random.seed(0)
     relations = random.sample(relations, 30)
-    net = create_graph(relations, paper=True)
+    net = create_graph(relations, color_keywords=True)
+    # print(net.generate_html())
     net.show(os.path.join(repo_path, 'key_vis.html'))
     webbrowser.open('file://' + os.path.join(repo_path,
                     'key_vis.html'), new=0, autoraise=True)
