1f682d415bff0f41caffb929b6fe3215425ca57f
uerib <simeon.allmendinger@student.kit.edu>
2022-06-09 18:31:49 +0200

    delete build_dir

--- build/lib/visanz/reichsanzeiger/test_aiohttp.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-url = "file:///Users/joel/Library/CloudStorage/OneDrive-Personal/_UNI/SS22/daVinci/src/05_Visualization/Models/VisualAnzeights.html"
-
-webbrowser.open(url, new=0, autoraise=True)
