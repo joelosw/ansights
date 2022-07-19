a377d9369c2a422ff852302bd18638f89ce4aabe
joelosw <joel.oswald@psiori.com>
2022-07-19 17:56:17 +0200

    more docstrings...

--- src/visanz/ner/keyword_extractor.py
+++ src/visanz/ner/keyword_extractor.py
@@ -1,3 +1,4 @@
+
 import yake
 import spacy
 from spacy import displacy
@@ -18,7 +19,28 @@ custom_kw_extractor = yake.KeywordExtractor(
     lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
 
 
-def extract_keyword_score(text, th=0.7):
+def extract_keyword_score(text: str, th: float = 0.7):
+    """
+    The extract_keyword_score function accepts a string of text and returns a list of keywords along with their scores.
+    Only keywords with a score<th are returned. 
+    (Lower score means more important)  
+
+    Parameters
+    ----------
+        text:str
+            Pass in the text that you want to extract keywords from
+        th:float=0.7
+            Set the threshold for the keywords
+
+    Returns
+    -------
+
+        A list of tuples, where each tuple contains a keyword and its score
+
+    Doc Author
+    ----------
+        Trelent
+    """
     keywords = custom_kw_extractor.extract_keywords(text)
     print("===== Keyword scores: {keywords}")
     keywords.sort(key=lambda x: x[1], reverse=False)
